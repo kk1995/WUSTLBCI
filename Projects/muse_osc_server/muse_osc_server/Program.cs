5f2041c274f366526f45e3851a7431ee8d0d3e82
kk1995 <b.kim@wustl.edu>
2016-09-24 12:21:15 -0500

    Add muse_osc_server that listens to MUSE port
    downloaded from muse website. slight mod made.

---
+++ Projects/muse_osc_server/muse_osc_server/Program.cs
@@ -0,0 +1,43 @@
+﻿// Tiny bit of mod from Kenny Kim
+
+using System;
+using SharpOSC;
+
+namespace muse_osc_server
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			// Callback function for received OSC messages. 
+			// Prints EEG and Relative Alpha data only.
+			HandleOscPacket callback = delegate(OscPacket packet)
+			{
+				var messageReceived = (OscMessage)packet;
+				var addr = messageReceived.Address;
+				if(addr == "/muse/eeg") {
+
+                    Console.Write("EEG values: ");
+					foreach(var arg in messageReceived.Arguments) {
+						Console.Write(arg + " ");
+					}
+                    Console.WriteLine();
+				}
+				if(addr == "/muse/elements/alpha_relative") {
+                    Console.Write("Relative Alpha power values: ");
+					foreach(var arg in messageReceived.Arguments) {
+						Console.Write(arg + " ");
+					}
+                    Console.WriteLine();
+                }
+			};
+
+			// Create an OSC server.
+			var listener = new UDPListener(5000, callback);
+
+			Console.WriteLine("Press enter to stop");
+			Console.ReadLine();
+			listener.Close();
+		}
+	}
+}
