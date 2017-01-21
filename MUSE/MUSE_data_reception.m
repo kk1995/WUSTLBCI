c9265582fcacfd8bc981e4f03affe8f2e8e5c562
kk1995 <b.kim@wustl.edu>
2017-01-21 17:34:34 -0600

    MUSE_data_reception now logs received data
    dataReceived has the data from MUSE.
    The exact format of the data is still unknown.
    
    muselab_recording.muse is a sample MUSE data recording.

--- MUSE/MUSE_data_reception.m
+++ MUSE/MUSE_data_reception.m
@@ -1,15 +1,20 @@
+clear all;
+clc;
+
+
 hudpr = dsp.UDPReceiver('LocalIPPort',5000);
 
 setup(hudpr);
-
-set(gcf,'CurrentCharacter','@'); % dummy character
 exit = 0;
+return_num = 0;
+bytesReceived = 0;
+dataReceived = [];
+
 while exit == 0
-  % check for keys
-  k=get(gcf,'CurrentCharacter');
-  if k~='@' % has it changed from the dummy character?
-    set(gcf,'CurrentCharacter','@'); % reset the character
-    % now process the key as required
-    if k=='q', exit=1; end
-  end
+   dataReceived_temp = step(hudpr);
+   dataReceived_temp2 = reshape(dataReceived_temp, [4 size(dataReceived_temp,1)/4]);
+   dataReceived_temp2
+   dataReceived = [dataReceived dataReceived_temp2];
+   bytesReceived = bytesReceived + length(dataReceived_temp);
+   return_num = return_num + 1;
 end
